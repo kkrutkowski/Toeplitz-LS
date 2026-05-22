c11b6beb20568a9d4bd08f506459c8a3374f6f89
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2026-05-22 17:25:48 +0200

    Adjust generic scaling constants for pswf43

--- include/scaling_generic.h
+++ include/scaling_generic.h
@@ -1,23 +1,23 @@
 #define F_ALPHA 0.20
 #define F_LRA_BETA 1.5
 #define F_LRA_GAMMA 0.65
-#define F_PSWF21_BETA 5.0
-#define F_PSWF21_GAMMA 7.5
-#define F_PSWF43_BETA 5.0
-#define F_PSWF43_GAMMA 7.5
+#define F_PSWF21_BETA 2.5
+#define F_PSWF21_GAMMA 10.0
+#define F_PSWF43_BETA 4.5
+#define F_PSWF43_GAMMA 20.0
 
 #define D_ALPHA 0.20
 #define D_LRA_BETA 1.5
 #define D_LRA_GAMMA 1.25
-#define D_PSWF21_BETA 10.0
+#define D_PSWF21_BETA 2.5
 #define D_PSWF21_GAMMA 10.0
-#define D_PSWF43_BETA 10.0
-#define D_PSWF43_GAMMA 10.0
+#define D_PSWF43_BETA 4.5
+#define D_PSWF43_GAMMA 40.0
 
 #define DD_ALPHA 0.11
 #define DD_LRA_BETA 0.65
 #define DD_LRA_GAMMA 0.135
-#define DD_PSWF21_BETA 6.5
+#define DD_PSWF21_BETA 2.5
 #define DD_PSWF21_GAMMA 100.0
-#define DD_PSWF43_BETA 6.5
-#define DD_PSWF43_GAMMA 100.0
+#define DD_PSWF43_BETA 3.75
+#define DD_PSWF43_GAMMA 450.0
