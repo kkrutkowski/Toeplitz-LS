399745845707bbd3b36d5f2ba89f165ad537f59f
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2026-05-24 18:04:59 +0200

    Add gitignore

--- examples/8_Benchmark/run_benchmark.sh
+++ examples/8_Benchmark/run_benchmark.sh
@@ -6,4 +6,6 @@
 python3 ./run_benchmark.py \
   --methods tlsf,fastchi2_103,aovdist,nifty \
   --dataset-limit 0 \
-  --compute-limit 16
+  --compute-limit 30
+
+python3 ./compare.py --index-path /home/krutkowski/Pulpit/Toeplitz-LS/Toeplitz-ls/examples/8_Benchmark/index.tsv --result-dir /home/krutkowski/Pulpit/Toeplitz-LS/Toeplitz-ls/examples/8_Benchmark/out
